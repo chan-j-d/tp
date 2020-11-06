178532b8727f8b384bbbf3590f1e0ec5b148f206
chan-j-d <chan.j.d.98@gmail.com>
2020-11-06 15:45:49 +0800

    Update TreeModel class to use Tag implementation

--- src/main/java/seedu/address/ui/taggui/TreeModel.java
+++ src/main/java/seedu/address/ui/taggui/TreeModel.java
@@ -9,15 +9,8 @@ import java.util.Map;
 
 public class TreeModel {
 
-    Cell graphParent;
-
-    List<Cell> allCells;
-    List<Cell> addedCells;
-    List<Cell> removedCells;
-
-    List<Edge> allEdges;
-    List<Edge> addedEdges;
-    List<Edge> removedEdges;
+    List<Cell> cells;
+    List<Edge> edges;
 
     Map<Tag,Cell> cellMap; // <id,cell>
 
@@ -28,113 +21,28 @@ public class TreeModel {
     }
 
     public void clear() {
-
-        allCells = new ArrayList<>();
-        addedCells = new ArrayList<>();
-        removedCells = new ArrayList<>();
-
-        allEdges = new ArrayList<>();
-        addedEdges = new ArrayList<>();
-        removedEdges = new ArrayList<>();
-
+        cells = new ArrayList<>();
+        edges = new ArrayList<>();
         cellMap = new HashMap<>(); // <id,cell>
+    }
+
+    public void addCell(Tag tag, String contactDetails) {
+        TagCell tagCell = new TagCell(tag, contactDetails);
+        cells.add(tagCell);
+
+        cellMap.put(tagCell.getTag(), tagCell);
 
     }
 
+    public void addEdge(Tag parentTag, Tag childTag) {
 
-    public void clearAddedLists() {
-        addedCells.clear();
-        addedEdges.clear();
-    }
-
-    public List<Cell> getAddedCells() {
-        return addedCells;
-    }
-
-    public List<Cell> getRemovedCells() {
-        return removedCells;
-    }
-
-    public List<Cell> getAllCells() {
-        return allCells;
-    }
-
-    public List<Edge> getAddedEdges() {
-        return addedEdges;
-    }
-
-    public List<Edge> getRemovedEdges() {
-        return removedEdges;
-    }
-
-    public List<Edge> getAllEdges() {
-        return allEdges;
-    }
-
-    public void addCell(String id) {
-
-
-    }
-
-    private void addCell( Cell cell) {
-
-        addedCells.add(cell);
-
-        cellMap.put( cell.getTag(), cell);
-
-    }
-
-    public void addEdge( String sourceId, String targetId) {
-
-        Cell sourceCell = cellMap.get( sourceId);
-        Cell targetCell = cellMap.get( targetId);
+        Cell sourceCell = cellMap.get(parentTag);
+        Cell targetCell = cellMap.get(childTag);
 
         Edge edge = new Edge( sourceCell, targetCell);
 
-        addedEdges.add( edge);
-
+        edges.add(edge);
     }
 
-    /**
-     * Attach all cells which don't have a parent to graphParent
-     * @param cellList
-     */
-    public void attachOrphansToGraphParent( List<Cell> cellList) {
 
-        for( Cell cell: cellList) {
-            if( cell.getCellParents().size() == 0) {
-                graphParent.addCellChild( cell);
-            }
-        }
-
-    }
-
-    /**
-     * Remove the graphParent reference if it is set
-     * @param cellList
-     */
-    public void disconnectFromGraphParent( List<Cell> cellList) {
-
-        for( Cell cell: cellList) {
-            graphParent.removeCellChild( cell);
-        }
-    }
-
-    public void merge() {
-
-        // cells
-        allCells.addAll( addedCells);
-        allCells.removeAll( removedCells);
-
-        addedCells.clear();
-        removedCells.clear();
-
-        // edges
-        allEdges.addAll( addedEdges);
-        allEdges.removeAll( removedEdges);
-
-        addedEdges.clear();
-        removedEdges.clear();
-
-    }
 }
